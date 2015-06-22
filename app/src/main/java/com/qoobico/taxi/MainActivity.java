5698ca255a7651c96b4dd01198a6caf3372a11db
SergeyPotemkin <yenon@yandex.ru>
2015-06-22 21:48:32 +0300

    test2 commit

---
+++ app/src/main/java/com/qoobico/taxi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.qoobico.taxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Admin on 22.06.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
